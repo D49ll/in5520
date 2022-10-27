eebbb6423bab05b5e6adc71d05b6d63ff0bfd53d
Daniel <daniel.stangeland@outlook.com>
2022-10-27 22:33:06 +0200

    finished assignment2

---
+++ assignments/assignment2/glcm.py
@@ -0,0 +1,142 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numba import jit
+from tqdm import tqdm
+
+from img_utils import requantize
+from img_utils import SlidingWindowIter
+
+## CODE TAKEN FROM SOLUTION GIVEN TO EXERCISES ##
+
+@jit(nopython=True)
+def glcm(quantized, dy, dx, gray_levels=256, normalise=True, symmetric = True):
+    glcm = np.zeros((gray_levels, gray_levels))
+    for y in range(quantized.shape[0] - abs(dy)):
+        for x in range(quantized.shape[1] - abs(dx)):
+            px1 = quantized[y, x]
+            px2 = quantized[y + dy, x + dx]
+            glcm[px1, px2] += 1
+
+    if normalise and symmetric:
+        glcm += np.transpose(glcm) #Added this to the code
+
+        return glcm * (1 / np.sum(glcm))
+
+    if normalise:
+        return glcm * (1 / np.sum(glcm))
+        
+    return glcm
+
+def compute_glcm_features(image, window_size, dy, dx, gray_levels, feature_fn):
+    feature_image = np.zeros(image.shape)
+
+    quantized = requantize(image, gray_levels)
+
+    for x, y, window in tqdm(SlidingWindowIter(quantized, window_size), colour='green'):
+        matrix = glcm(window, dy, dx, gray_levels)
+        feature_image[y, x] = feature_fn(matrix)
+    return feature_image
+
+## CODE CREATED FOR ASSIGMENT 2 ##
+def compute_quadrant_features(image, window_size, dy, dx, gray_levels, quadrant):
+    feature_image = np.zeros(image.shape)
+    quantized = requantize(image, gray_levels)
+
+    for x, y, window in tqdm(SlidingWindowIter(quantized, window_size)):
+        matrix = glcm(window, dy, dx, gray_levels)
+        feature_image[y, x] = quadruant(matrix, quadrant)
+    return feature_image
+
+
+@jit(nopython=True)
+def quadruant(glcm, quadrant=1):
+    norm = (np.sum(glcm[:8,:8])+np.sum(glcm[8:,:8])+np.sum(glcm[8:,8:]))
+    sub = glcm[:8,:8]
+
+    if quadrant == 1:
+        return np.sum(sub[:4,:4])/np.sum(sub)
+    if quadrant == 2:
+        #sub Q2 of original Q1
+        return np.sum(sub[:4,4:])/np.sum(sub)
+    if quadrant == 3:
+        #sub Q3 of original Q1
+        return np.sum(sub[4:,:4])/np.sum(sub)
+    if quadrant == 4:
+        #sub Q4 of original Q1
+        return np.sum(sub[4:,4:])/np.sum(sub)
+    if quadrant == 5:
+        return np.sum(glcm[:8,8:])/norm
+    if quadrant == 6:
+        return np.sum(glcm[8:,:8])/norm
+    if quadrant == 7:
+        return np.sum(glcm[8:, 8:])/norm
+
+## CODE CREATED FOR ASSIGNMENT 1 ##
+
+@jit(nopython=True)
+def inertia(matrix):
+    inr = 0
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            inr += matrix[i, j] * (i - j)**2
+    return inr
+
+@jit (nopython=True)
+def homogeneity(matrix):
+    idm = 0
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            idm += matrix[i, j] * (1 / (1+(i-j)**2))
+    return idm
+
+@jit (nopython = True)
+def px(i, matrix):
+    P = 0
+    for j in range(matrix.shape[0]):
+        P += matrix[i, j]
+    return P
+
+@jit (nopython = True)
+def py(i, matrix):
+    P = 0
+    for j in range(matrix.shape[0]):
+        P += matrix[j, i]
+    return P
+
+@jit (nopython = True)
+def ux(matrix):
+    u = 0
+    for i in range(matrix.shape[0]):
+        u += i * px(i, matrix)
+    return u
+
+@jit (nopython = True)
+def uy(matrix):
+    u = 0
+    for i in range(matrix.shape[0]):
+        u += i * py(i, matrix)
+    return u
+
+@jit (nopython = True)
+def cluster_shade(matrix):
+    u_x = ux(matrix)
+    u_y = uy(matrix)
+
+    shd = 0
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            shd += matrix[i, j] * (i+j-u_x-u_y)**3
+    return shd
+
+def isotropic_glcm(image, gray_levels, distance=1):
+    quant_image = requantize(image, gray_levels=gray_levels)
+    glcm_0 = glcm(quant_image, dx=distance, dy=0, gray_levels=gray_levels, normalise=True)
+    glcm_45 = glcm(quant_image, dx=distance, dy=distance, gray_levels=gray_levels, normalise=True)
+    glcm_90 = glcm(quant_image, dx=0, dy=distance, gray_levels=gray_levels, normalise=True)
+    glcm_135 = glcm(quant_image, dx=-distance, dy=distance, gray_levels=gray_levels, normalise=True)
+
+    isotropic = (glcm_0+glcm_45+glcm_90+glcm_135) / 4
+
+    return isotropic
+
+
---
+++ assignments/assignment2/img_utils.py
@@ -0,0 +1,114 @@
+import numpy as np
+from seaborn import heatmap
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+## CODE TAKEN FROM SOLUTION GIVEN TO EXERCISES ##
+
+class SlidingWindowIter:
+    def __init__(self, image, window_size):
+        self.wsize = window_size
+        self.x = 0
+        self.y = 0
+        self.count = 0
+        self.pad = (window_size - 1) // 2
+        self.img_dims = image.shape
+        self.padded = np.pad(
+            image,
+            ((self.pad, self.pad), (self.pad, self.pad)),
+            mode="reflect",
+        )
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.y >= self.img_dims[0] + self.pad - 1:
+            raise StopIteration
+        else:
+            self.x = (self.count) % self.img_dims[1] + self.pad
+            self.y = self.count // self.img_dims[0] + self.pad
+            self.count += 1
+            return (
+                self.x - self.pad,
+                self.y - self.pad,
+                self.padded[
+                    self.y - self.pad : self.y + self.pad + 1,
+                    self.x - self.pad : self.x + self.pad + 1,
+                ],
+            )
+
+def requantize(img, gray_levels):
+    return np.uint8(np.round(img * ((gray_levels - 1) / 255)))
+
+def threshhold(img, T, reverse=True):
+    """
+    Simple global threshhold
+    """
+
+    thresholded = np.zeros(img.shape, dtype=np.uint8)
+    for index, val in np.ndenumerate(img):
+        if not reverse:
+            if val < T:
+                thresholded[index[0], index[1]] = 1
+        else:
+            if val > T:
+                thresholded[index[0], index[1]] = 1
+    return thresholded
+
+## CODE CREATED FOR THIS ASSIGNMENT ##
+
+def save_image(image, glcm_image, gray_levels, dx, dy, filename, texture_start):
+    fig = plt.figure(figsize=(20,10))
+    ax1, ax2 = fig.subplots(2,4)   
+    
+    for i in range(4):
+        ax1[i].axis('off')
+        ax1[i].set_aspect(1)
+
+        ax2[i].axis('off')
+        ax2[i].set_aspect(1)
+
+    ax1[0].imshow(image[0], cmap=plt.cm.gray)
+    ax1[0].set_title(f'texture {texture_start+1}')
+
+    heatmap(glcm_image[0],ax=ax1[1])
+    ax1[1].set_title(f'dx: {dx[0]}, dy: {dy[0]}')
+
+    ax1[2].imshow(image[1], cmap=plt.cm.gray)
+    ax1[2].set_title(f'texture {texture_start+2}')
+
+    heatmap(glcm_image[1],ax=ax1[3])
+    ax1[3].set_title(f'dx: {dx[1]}, dy: {dy[1]}')
+
+    ax2[0].imshow(image[2],cmap=plt.cm.gray)
+    ax2[0].set_title(f'texture {texture_start+3}')
+
+    heatmap(glcm_image[2],ax=ax2[1])
+    ax2[1].set_title(f'dx: {dx[2]}, dy: {dy[2]}')
+        
+    ax2[2].imshow(image[3],cmap=plt.cm.gray)
+    ax2[2].set_title(f'texture {texture_start+4}')
+
+    heatmap(glcm_image[3],ax=ax2[3])
+    ax2[3].set_title(f'dx: {dx[3]}, dy: {dy[3]}')
+
+    fig.suptitle(f'GLCM of {filename} with gray_levels={gray_levels}')
+    plt.savefig(f"{filename}_glcms.png") 
+
+def read_image(filename):
+    f = np.asarray(Image.open(filename))
+    return f
+
+def sub_images(image):
+    return np.array([image[:256, :256], image[:256,256:], image[256:,:256], image[256:,256:]],dtype=np.uint8)
+
+def save_images(images,filename):
+    for i in range(len(images)):
+        Image.fromarray(images[i]).save(filename+"_texture"+str(i+1)+".png")
+
+def extract_subimages(filename):
+    img = read_image(filename+'.png')
+    sub = sub_images(img)
+    return sub
